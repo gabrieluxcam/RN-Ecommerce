9500274e0fa0a338c2b68c649c7f4196400e963d
Gabriel Madera <gabriel@uxcam.com>
2023-03-17 13:21:31 +0100

    set up UXCAM_KEY for sharing

--- App.tsx
+++ App.tsx
@@ -23,6 +23,10 @@ RNUxcam.startWithConfiguration(configuration);
 const App = () => {
   LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
   LogBox.ignoreAllLogs(); //Ignore all log notifications
+  console.log('====================================');
+  console.log('uxcam key coming');
+  console.log('====================================');
+  console.log(UXCAM_API_KEY);
 
   return (
     <ClothesProvider>
